8d5b1097780e86847688b4496fa802e295a298ad
noor <gurnoor.bola@gmail.com>
2026-05-17 17:53:02 -0700

    fixed concurrency bug again

--- Queue.hpp
+++ Queue.hpp
@@ -1,3 +1,7 @@
+/**
+ * A Single Reader Single Writer Lock Free Queue Implementation
+ */
+
 #ifndef QUEUE_H
 #define QUEUE_H
 
@@ -28,42 +32,42 @@ class Queue {
     std::array<T, N+1> m_buffer{};
 };
 
-#endif
-
-#ifdef QUEUE_IMPL
-
-#include <algorithm>
 
 template <typename T, std::size_t N>
 template <std::size_t I>
 std::size_t Queue<T, N>::push(std::array<T, I>& src, std::size_t n, std::size_t offset) {
-    std::size_t front{m_front.load()};
+    std::size_t front{m_front.load(std::memory_order_acquire)};
+    std::size_t back{m_back.load(std::memory_order_relaxed)};
 
     std::size_t numPushed{0};
     for (; numPushed < n; ++numPushed) {
-        std::size_t next {(m_back + 1) % (N + 1)};
+        std::size_t next {(back + 1) % (N + 1)};
         if (next == front) break;
         m_buffer[next] = src[offset++];
-        m_back = next;
+        back = next;
     }
 
+    m_back.store(back, std::memory_order_release);
     return numPushed;
 }
 
 template <typename T, std::size_t N>
 template <std::size_t I>
 std::size_t Queue<T, N>::pop(std::array<T, I>& dest, std::size_t n, std::size_t offset) {
-    std::size_t back{m_back.load()};
-    if (m_front == back) return 0;
+    std::size_t back{m_back.load(std::memory_order_acquire)};
+    std::size_t front{m_front.load(std::memory_order_relaxed)};
+    if (front == back) return 0;
 
     std::size_t numPopped{0};
-    for (; numPopped < n; ++numPopped) {
-        std::size_t next = {(m_front + 1) % (N + 1)}; 
+    for (int i{0}; i < n; ++i) {
+        std::size_t next = {(front + 1) % (N + 1)}; 
         dest[offset++] = m_buffer[next];
-        m_front = next;
-        if (m_front == back) return numPopped + 1;
+        front = next;
+        ++numPopped;
+        if (front == back) break;
     }
 
+    m_front.store(front, std::memory_order_release);
     return numPopped;
 }
 
